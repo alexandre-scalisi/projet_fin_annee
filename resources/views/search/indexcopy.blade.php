b14dbe02f83a71e4d1ea13699f9ed9625d74ecb4
alexlemia13 <alexlemia13@outlook.fr>
2021-06-22 08:55:52 +0200

    .

--- resources/views/search/indexcopy.blade.php
+++ resources/views/search/indexcopy.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
+    {{-- {{ dd("".arequest()->query()) }} --}}
     {{-- {{ dd(request()->query()['order_by']) }} --}}
     <div class="w-full rounded-lg bg-gray-300 p-4 mb-4">
-        <form method="GET">
+        <form method="GET" id="form">
 
             <input type="text" name="q" placeholder="Titre" class="block">
             <x-genre-modal />
@@ -33,6 +34,7 @@
                     <option value="release_date">date de sortie &downarrow;</option>
                     <option value="upload_date">rajouté le &downarrow;</option>
                 </select>
+                <input type="hidden" name="l" id="letter">
                 <input type="checkbox" name="d" id="direction" {{ isset(request()->query()['d']) && request()->query()['d'] === 'on'  ? 'checked' : '' }}>
                 <label for="direction">inverser direction</label>
             </div>
@@ -44,8 +46,8 @@
     </div>
     <div class="w-full rounded-lg bg-gray-400 p-4">
         <div class="flex space-x-3 flex-wrap" x-data="">
-
-            <form x-ref="form" method="GET" action={{ route('search.index') }}>
+            
+            {{-- <form x-ref="form" method="GET" action={{ route('search.index') }}>
                 <button type="submit" name="l" value="tous" class="bg-blue-500 px-2 text-gray-100 mb-2">tous</button>
                 <button type="submit" name="l" value="autres"
                     class="bg-blue-500 px-2 text-gray-100 mb-2">autres</button>
@@ -53,7 +55,11 @@
                 <button name="l" type="submit" value="{{ $l }}"
                     class="bg-blue-500 px-2 text-gray-100 mb-2">{{ $l }}</button>
                 @endforeach
-            </form>
+            </form> --}}
+            @foreach(array_merge(['tous', 'autres'], range('a','z')) as $l)
+            <button type="button" value="{{ $l }}" class="bg-blue-500 px-2 text-gray-100 mb-2"
+            onclick="document.getElementById('letter').value = this.value; document.getElementById('form').submit()">{{ $l }}</button>
+           @endforeach
         </div>
         @foreach($array as $arr)
         <p><a href="{{ route('animes.show', $arr->id) }}" class="text-blue-600 mb-3">{{ $arr->title }}</a></p>
