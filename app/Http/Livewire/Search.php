23754a844581ee01bec451eccd8ad4d2bb6c6265
alexlemia13 <alexlemia13@outlook.fr>
2021-06-30 21:47:35 +0200

    .

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -10,8 +10,33 @@ class Search extends Component
     public $search;
     public $results;
 
+    public function mount() {
+        
+
+        
+    //     $result = Anime::orderBy('title')->paginate();
+    //     $sorted_result =$result->getCollection()->sort(function($a, $b) {
+    //             return $a->title[0] !== 'A';  
+    //    })->values();
+
+
+    }
     public function test() {
-       $this->results = Anime::orderBy('title', 'asc')->where('title','LIKE',$this->search.'%')->take(5)->get();
+        
+        $array_words = explode(' ', $this->search);
+        $new = collect($array_words)->reduce(function($a, $b) {
+            return $a . "%$b%";
+        });
+        if($this->search === '')
+            return;
+        // if(empty($this->search))
+            // return;
+        $first_letter = $this->search[0] ?? '';
+    //    $this->results = Anime::orderBy('title', 'asc')->where('title','LIKE',$new)->take(5)->get();
+       $this->results = Anime::orderBy('title')->where('title', 'LIKE', $new)->get()->sort(function($a, $b) use( $first_letter ) {
+        // return $a[0]->title[0] !== "A";
+        return ucfirst($a->title[0]) !== ucfirst($first_letter);
+    })->take(5);
     }
 
     public function render()
