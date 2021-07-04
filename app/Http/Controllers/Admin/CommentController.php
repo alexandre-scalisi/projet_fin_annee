022fa086ba2ec3aca8ca3ccb217b9498bfccd4a6
alexlemia13 <alexlemia13@outlook.fr>
2021-07-04 14:55:10 +0200

    .

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -21,22 +21,21 @@ class CommentController extends Controller
     }
 
     private function search() {
-        $order_by = lcfirst(request()->input('order_by', 'author'));
-        $dir = lcfirst(request()->input('dir', 'asc'));
-        $accepted_order_bys = ['author', 'title', 'created_at'];
+        $order_by = lcfirst(request()->input('order_by', 'created_at'));
+        $dir = lcfirst(request()->input('dir', 'desc'));
+        $accepted_order_bys = ['author', 'created_at'];
         $accepted_dirs =['asc', 'desc'];
         if(!in_array($order_by, $accepted_order_bys))
-            $order_by = 'author';
+            $order_by = 'created_at';
         
         if(!in_array($dir, $accepted_dirs))
             $dir = 'asc';
-        if($order_by = 'author')
+        if($order_by == 'author')
             
             return Comment::join('users', 'users.id', '=', 'comments.user_id' )->
                 orderBy('users.email', $dir)
                 ->paginate(20);
             
-       
         return Comment::orderBy($order_by, $dir)->paginate(20);
         
     }
