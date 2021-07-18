80995b00f2cc075e304f26c51a48000965363a2b
alexlemia13 <alexlemia13@outlook.fr>
2021-07-18 15:04:14 +0200

    .

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -1,3 +1,4 @@
+@props(['top' => '-20px', 'left' => 0])
 <div class="absolute bottom-0 border border-black bg-white px-2 py-2 text-sm" style="left: {{ $left }}; transform: translateY({{ $top }})" x-show="tooltip">
     <p class="text-gray-800 text-sm font-sans font-normal">
         {{ $slot }}
