76126f445b22c76c17819db12c5c7f82e84880d7
alexlemia13 <alexlemia13@outlook.fr>
2021-07-20 19:13:59 +0200

    .

--- resources/views/anime/show.blade.php
+++ resources/views/anime/show.blade.php
@@ -1,75 +1,76 @@
 <x-app-layout>
-@php
-    $last_episode_id = $anime->episodes->pluck('id')->toArray()[count($anime->episodes) - 1];
+    <div class="-mx-4 sm:mx-0">
+        @php
+        $last_episode_id = $anime->episodes->pluck('id')->toArray()[count($anime->episodes) - 1];
 
-    @endphp
+        @endphp
 
-    <div class="anime-header">
-        <div class="anime__image" style="background-image: url({{ h_find_image($anime->image) }})">
-            
+        <div class="anime-header">
+            <div class="anime__image" style="background-image: url({{ h_find_image($anime->image) }})">
 
-        </div>
-
-        <div class="anime-header__content-container">
-            <div class="anime__title_container">
-                <h1 class="anime__title">{{ $anime->title }}</h1>
-                {{-- TODO Rajouter tooltip et lien vers favoris --}}
 
             </div>
-            <ul>
-                <li class="anime__info"><span class="anime__info_big">Date de sortie
-                        :</span> {{ Carbon\Carbon::parse( $anime->release_date)->translatedFormat('d F Y') }}
-                </li>
-                <li class="anime__info"><span class="anime__info_big">Studio :</span>
-                    {{ $anime->studio }}</li>
-                <li class="anime__info"><span class="anime__info_big">Genres :</span>
-                    {{ implode(', ', $anime->genres->pluck('name')->toArray()) }}</li>
-                <li class="anime__info inline-block mr-12"><span class="anime__info_big">Episodes :</span>
-                    {{ count($anime->episodes) }}</li>
-                @livewire('stars', compact('anime'))
-            </ul>
-            
-            <div class="mt-6 flex items-center flex-wrap gap-y-4">
-                <a href="{{ route('animes.episodes.show', ['anime' => $anime->id, 'episode' => $anime->episodes->first()->id] ) }}"
-                    class="anime__btn anime__btn_first">Premier EP</a>
-                <a href="{{ route('animes.episodes.show', ['anime' => $anime->id, 'episode' => $last_episode_id] ) }}"
-                    class="anime__btn mr-auto">Dernier EP</a>    
-                @auth                
-                 @livewire('follow-button', compact('anime'))
-                @endauth
-                <a href="#form" class="anime__comment-link">{{ $anime->comments->count() }} commentaires</a>
+
+            <div class="anime-header__content-container">
+                <div class="anime__title_container">
+                    <h1 class="anime__title">{{ $anime->title }}</h1>
+                    {{-- TODO Rajouter tooltip et lien vers favoris --}}
+
+                </div>
+                <ul>
+                    <li class="anime__info"><span class="anime__info_big">Date de sortie
+                            :</span> {{ Carbon\Carbon::parse( $anime->release_date)->translatedFormat('d F Y') }}
+                    </li>
+                    <li class="anime__info"><span class="anime__info_big">Studio :</span>
+                        {{ $anime->studio }}</li>
+                    <li class="anime__info"><span class="anime__info_big">Genres :</span>
+                        {{ implode(', ', $anime->genres->pluck('name')->toArray()) }}</li>
+                    <li class="anime__info inline-block mr-12"><span class="anime__info_big">Episodes :</span>
+                        {{ count($anime->episodes) }}</li>
+                    @livewire('stars', compact('anime'))
+                </ul>
+
+                <div class="mt-6 flex items-center flex-wrap gap-y-4">
+                    <a href="{{ route('animes.episodes.show', ['anime' => $anime->id, 'episode' => $anime->episodes->first()->id] ) }}"
+                        class="anime__btn anime__btn_first">Premier EP</a>
+                    <a href="{{ route('animes.episodes.show', ['anime' => $anime->id, 'episode' => $last_episode_id] ) }}"
+                        class="anime__btn mr-auto">Dernier EP</a>
+                    @auth
+                    @livewire('follow-button', compact('anime'))
+                    @endauth
+                    <a href="#form" class="anime__comment-link">{{ $anime->comments->count() }} commentaires</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div x-data="{truncated: true }" class="synopsis">
+            <h2 class="synopsis__title">Synopsis</h2>
+            <p class="synopsis__text" x-show="truncated"> {{ $truncated_synopsis }} </p>
+            <p class="synopsis__text" x-show="!truncated"> {{ $anime->synopsis }} </p>
+            @if($anime->synopsis !== $truncated_synopsis)
+            <btn class="btn-more" @click="truncated = !truncated"
+                x-text="truncated ? 'Afficher plus' : 'Afficher moins'"></btn>
+            @endif
+
+        </div>
+        <div class="anime-episodes">
+            <h2 class="anime-episodes__title">
+                Liste des épisodes
+            </h2>
+            <div class="anime-episodes__list">
+                <ul>
+                    @foreach ($episodes as $episode)
+                    <li>
+                        <a href="{{ route('animes.episodes.show', ['anime' => $anime->id, 'episode' => $episode->id] ) }}"
+                            class="anime-episodes__item">{{ $episode->title }}</a>
+                    </li>
+                    @endforeach
+                </ul>
+                {{ $episodes->links() }}
             </div>
         </div>
-
+        <!-- TODO gerer la pagination -->
+        @livewire('form-container', ['type_id' => $anime->id, 'type' => 'Anime'])
     </div>
-
-    <div x-data="{truncated: true }" class="synopsis">
-        <h2 class="synopsis__title">Synopsis</h2>
-        <p class="synopsis__text" x-show="truncated"> {{ $truncated_synopsis }} </p>
-        <p class="synopsis__text" x-show="!truncated"> {{ $anime->synopsis }} </p>
-        @if($anime->synopsis !== $truncated_synopsis)
-        <btn class="btn-more" @click="truncated = !truncated"
-            x-text="truncated ? 'Afficher plus' : 'Afficher moins'"></btn>
-        @endif
-
-    </div>
-    <div class="anime-episodes">
-        <h2 class="anime-episodes__title">
-            Liste des épisodes
-        </h2>
-        <div class="anime-episodes__list">
-            <ul>
-                @foreach ($episodes as $episode)
-                <li>
-                    <a href="{{ route('animes.episodes.show', ['anime' => $anime->id, 'episode' => $episode->id] ) }}"
-                        class="anime-episodes__item">{{ $episode->title }}</a>
-                </li>
-                @endforeach
-            </ul>
-            {{ $episodes->links() }}
-        </div>
-    </div>
-    <!-- TODO gerer la pagination -->
-    @livewire('form-container', ['type_id' => $anime->id, 'type' => 'Anime'])
-
 </x-app-layout>
