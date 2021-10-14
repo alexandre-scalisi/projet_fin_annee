a98abb9b780bd6df6f9d51fb8f12f0064ca140a9
alexlemia13 <alexlemia13@outlook.fr>
2021-10-14 21:35:10 +0200

    mit à jour url github

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-center bg-blue-500 pt-8 pb-4">
     <ul class="flex justify-center items-center gap-3">
-        <li><a href="https://github.com/alexlemia13" class="fab fa-github fa-2x"><span class="sr-only">lien github</span></a></li>
+        <li><a href="https://github.com/alexandre-scalisi" class="fab fa-github fa-2x"><span class="sr-only">lien github</span></a></li>
         <li><a href="http://www.linkedin.com/in/alexandre-scalisi" class="fab fa-2x fa-linkedin-in"><span class="sr-only">lien linkedin</span></a></li>
         <li><a href="mailto:scalisialexandre@gmail.com" class="fas fa-at fa-2x"><span class="sr-only">lien email</span></a></li>
     </ul>
